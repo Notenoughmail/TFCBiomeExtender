2ed2b7c2e9b0064922744f6cbf06f686cf9e8668
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2025-02-04 21:45:54 -0700

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# TFCBiomeExtender
+
+A mod aimed at pack-devs that implements a version of TFC's chunk/world generator that can be customized via datapacks to a greater degree
---
+++ src/main/java/com/notenoughmail/tfcbiomeextender/TFCBiomeExtender.java
@@ -0,0 +1,9 @@
+package com.notenoughmail.tfcbiomeextender;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TFCBiomeExtender.ID)
+public class TFCBiomeExtender {
+
+    public static final String ID = "tfcbiomeextender";
+}
